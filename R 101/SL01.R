90ecb0d69f7182558331f31f68cd7cbb762e8a4a
denis parra <dalpasa@yahoo.com>
2016-08-04 16:42:01 -0400

    script R para demo
    version 0.001 (no significativa)

---
+++ R 101/SL01.R
@@ -0,0 +1,22 @@
+# Script para iniciar uso de R a través de un ejemplo con Second Life
+#
+# @date August 5, 2016
+# @author Denis Parra
+
+# Carguemos el archivo
+
+
+# ¿ Qué variables/campos/columnas describen a este archivo ?
+
+
+# Cómo veo un resumen rápido de esta información, por ejemplo
+# medidas de tendencia central ?
+
+
+# Es esa la mejor forma? (Hint: No -> paquete psych)
+
+# Podemos hacer un test de Hipotesis?
+
+# Y una regresion lineal?
+
+# Cómo visualizar el efecto de al menos una IV sobre la DV?
